e7977098ff0b2713fb3cec37ae371a84e3ca2267
Benoît Dls <benoit.dls@yahoo.com>
2024-01-24 10:04:54 +0100

    Initial commit

---
+++ src/Controller/LastCallController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class LastCallController extends AbstractController
+{
+    private $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    #[Route('/last_call', name: 'app_last_call')]
+    public function index(): Response
+    {
+        return $this->render('last_call/index.html.twig', [
+            'controller_name' => 'LastCallController',
+        ]);
+    }
+
+    #[Route('/last_call/station/{stop_area_id}', name: 'app_last_call_station')]
+    public function station(string $stop_area_id): Response
+    {
+        $response = $this->client->request(
+            'GET',
+            'https://api.navitia.io/v1/coverage/fr-idf/stop_areas/'.$stop_area_id.'/departures?',
+            [
+                'headers' => [
+                    'Authorization' => 'Basic MWI1YTUxNDItNjQzNS00MzI3LWFmNDMtNWM3MTBiMDVkOWMwOg==',
+                ],
+            ]
+        );
+
+        dump($response->toArray());
+
+        return $this->render('last_call/station.html.twig', [
+            'controller_name' => 'LastCallController',
+            'stop_area_id' => $stop_area_id
+        ]);
+    }
+}
---
+++ src/Entity/Favorite.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FavoriteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FavoriteRepository::class)]
+class Favorite
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $stop_point = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getStopPoint(): ?string
+    {
+        return $this->stop_point;
+    }
+
+    public function setStopPoint(string $stop_point): static
+    {
+        $this->stop_point = $stop_point;
+
+        return $this;
+    }
+}
